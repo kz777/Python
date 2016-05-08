684006d1543205000920affbe9a2a4901cb87d0a
ASLAN USSAYEV <usprof777@gmail.com>
2016-05-07 20:55:26 -0500

    Initial Commit

---
+++ trip_cost.py
@@ -0,0 +1,24 @@
+def hotel_cost(nights):
+    return 140 * nights
+    
+def plane_ride_cost(city):
+    if city == "Charlotte":
+        return 183
+    elif city == "Tampa":
+        return 220
+    elif city == "Pittsburgh":
+        return 222
+    elif city == "Los Angeles":
+        return 475
+
+def rental_car_cost(days):
+    rent = days * 40
+    if days >= 7:
+        rent -= 50
+    elif days >= 3 and days < 7:
+        rent -= 20
+    return rent
+    
+def trip_cost(city, days):
+    """ returns the sum of the previous three functions when called with days and city, respectively."""
+    return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city)
---
+++ true_or_false.py
@@ -0,0 +1,19 @@
+"""
+Assign True or False as appropriate for bool_one through bool_five.
+
+Set bool_one equal to the result of False or not True and True
+Set bool_two equal to the result of False and not True or True
+Set bool_three equal to the result of True and not (False or False)
+Set bool_four equal to the result of not not True or False and not True
+Set bool_five equal to the result of False or not (True and True)
+"""
+
+bool_one = False
+
+bool_two = True
+
+bool_three = True
+
+bool_four = True
+
+bool_five = False
